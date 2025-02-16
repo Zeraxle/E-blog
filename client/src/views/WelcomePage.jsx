b32f1438a4a031e29de6e14b64ad455a7c56e12e
Nathan Marrow <Code2Gain@gmail.com>
2025-02-15 19:15:51 -0500

    CSS

--- client/src/views/WelcomePage.jsx
+++ client/src/views/WelcomePage.jsx
@@ -1,12 +1,13 @@
-import {Link} from 'react-router-dom'
-
+import { Link } from 'react-router-dom'
+import './WelcomePage.css'
 
 export const WelcomePage = () => {
-
-    return(<>
-        <h1>Welcome to E-Blog</h1>
-        <p>Find your next favorite flick</p>
-        <Link to={'/login'}>Login</Link>
-        <Link to={'/register'}>Sign Up</Link>
-    </>)
+    return (
+        <div className="welcome-container">
+            <h1>Welcome to E-Blog</h1>
+            <p>Find your next favorite flick</p>
+            <Link to={'/login'}>Login</Link>
+            <Link to={'/register'}>Sign Up</Link>
+        </div>
+    );
 }
